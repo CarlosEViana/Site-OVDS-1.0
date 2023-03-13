5e709408adb24f123d75b25d1961b7a94822bae8
Carlos E Viana <carlosvianavidal@gmail.com>
2023-03-13 16:11:21 -0300

    Site-OVS
    Slider em js

--- index.js
+++ index.js
@@ -1,2 +1,35 @@
 
-console.log('Hello World')
+// slider   
+
+
+    const slider = document.querySelector('.slider');
+const slides = slider.querySelectorAll('.slide');
+const prevButton = document.querySelector('#prevButton');
+const nextButton = document.querySelector('#nextButton');
+let currentSlideIndex = 0;
+
+function showSlide(index) {
+  slides[currentSlideIndex].classList.add('hidden');
+  slides[index].classList.remove('hidden');
+  currentSlideIndex = index;
+}
+
+prevButton.addEventListener('click', () => {
+  let index = currentSlideIndex - 1;
+  if (index < 0) {
+    index = slides.length - 1;
+  }
+  showSlide(index);
+});
+
+nextButton.addEventListener('click', () => {
+  let index = currentSlideIndex + 1;
+  if (index >= slides.length) {
+    index = 0;
+  }
+  showSlide(index);
+});
+
+showSlide(currentSlideIndex);
+
+ 
